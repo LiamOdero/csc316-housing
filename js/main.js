47cfb88268fbca6d9db31da378e0699a3879375c
Liam Odero <liam.odero1@gmail.com>
2025-10-18 17:31:58 -0400

    initial setup

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// Start application by loading the data
+loadData();
+
+function loadData() {
+    d3.csv("data/star_dataset.csv"). then(data=>{
+
+    });
+}
+
+function prepareData(data){
+
+}
