623617795fa9f4932ad0201872ee4869a53100aa
Liam Odero <liam.odero1@gmail.com>
2025-10-19 14:27:00 -0400

    completed data cleaning process

--- js/main.js
+++ js/main.js
@@ -3,11 +3,29 @@
 loadData();
 
 function loadData() {
-    d3.csv("data/star_dataset.csv"). then(data=>{
-
+    d3.csv("data/avg_rent_by_pop.csv"). then(data=>{
+        cleaned_data = preparePopRentData(data);
     });
 }
 
-function prepareData(data){
+function preparePopRentData(data){
+    cleaned_data = []
 
+    data.forEach(e => {
+        let loc = e.GEO;
+
+        let index = loc.indexOf(",");
+        let city = loc.slice(0, index);
+        let dataObj = {structure: e["Type of structure"], unit: e["Type of unit"], avg: +e.VALUE}
+
+        if (cleaned_data.length > 0 && cleaned_data[cleaned_data.length - 1].city == city) {
+            cleaned_data[cleaned_data.length - 1].data.push(dataObj)
+        }   else    {
+            let province = loc.slice(index + 1);
+
+            let locObj = {year: e.REF_DATE, city: city, province: province, pop: +e.POP, data: [dataObj]};
+            cleaned_data.push(locObj)
+        }        
+    });
+    console.log(cleaned_data)
 }
